16588b9c182ceef071c35696f144c510ee68032e
luisgcastro <57987120+luisgcastro@users.noreply.github.com>
2021-02-18 12:54:33 +0100

    18/02

--- PSP/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module psp {
-	requires java.logging;
-	requires java.mail;
-	requires javax.mail;
-	requires mail;
-}
--- PSP/src/popsmtpftp/protocoloftp.java
+++ PSP/src/popsmtpftp/protocoloftp.java
@@ -1,9 +1,9 @@
 package popsmtpftp;
 
 import java.io.IOException;
+
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp;
 
 
 
